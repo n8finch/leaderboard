ff76f55442c67ea06d9d2dd4d3b9392c1ebe22de
Nate Finch <n8finch@Megan-Finchs-MacBook.local>
2015-05-13 14:21:09 +0900

    added methods to refactor the code a bit

--- leaderboard.js
+++ leaderboard.js
@@ -36,13 +36,13 @@ if (Meteor.isClient) {
 
     'click .increment': function() {
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+      Meteor.call('modifyPlayerScore', selectedPlayer, 5);
 
     },
 
     'click .decrement': function() {
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+      Meteor.call('modifyPlayerScore', selectedPlayer, -5);
 
     }
 
@@ -53,20 +53,13 @@ if (Meteor.isClient) {
     'submit form': function(){
       event.preventDefault();
       var playerNameVar = event.target.playerName.value;
-      var currentUserId = Meteor.userId();
-      PlayersList.insert({
-        name: playerNameVar,
-        score: 0,
-        createdBy: currentUserId
-
-      })
-      
+      Meteor.call('insertPlayerData', playerNameVar );
 
     },
 
     'click .remove': function() {
       var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.remove(selectedPlayer);
+      Meteor.call('removePlayerData', selectedPlayer);
     }
 
 
@@ -85,5 +78,27 @@ if (Meteor.isServer) {
 
   });
 
+  Meteor.methods({
+      'insertPlayerData': function(playerNameVar){
+        var currentUserId = Meteor.userId();
+        PlayersList.insert({
+          name: playerNameVar,
+          score: 0,
+          createdBy: currentUserId
+
+        })
+
+      },
+
+      'removePlayerData': function(selectedPlayer){
+        PlayersList.remove(selectedPlayer);
+      },
+
+      'modifyPlayerScore': function(selectedPlayer, scoreValue){
+        PlayersList.update(selectedPlayer, {$inc: {score: scoreValue}});
+
+      }
+
+  });
 
 }
